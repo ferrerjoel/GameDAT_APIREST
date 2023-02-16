395ff2bb4cdabe50908929db651e5c941398ecbc
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2023-02-16 20:40:32 +0100

    Added POST reads and routing

---
+++ routes.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const Post = require("./models/Post")
+const router = express.Router()
+
+// Get all posts
+router.post("/posts", async (req, res) => {
+	const post = new Post({
+        title: req.body.title,
+        launch_date: req.body.launch_date,
+        summary: req.body.summary,
+        pegi_rate: req.body.pegi_rate,
+        duration: req.body.duration
+	})
+	await post.save()
+	res.send(post)
+})
+
+module.exports = router 

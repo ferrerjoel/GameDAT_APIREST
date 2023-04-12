f8185f882efd581d54bc793373d6e3926324360c
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2023-04-12 13:02:46 +0200

    Added cors

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require("express")
 const mongoose = require("mongoose") 
 const routes = require("./routes")
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
 // Connect to MongoDB database
 mongoose
 	.connect("mongodb://127.0.0.1:27017/GameDAT", { useNewUrlParser: true })
 	.then(() => {
 		const app = express()
 		app.use(express.json())
+		app.use(cors(corsOptions)) // Use this after the variable declaration
 		app.use("/api", routes)
 		
 		app.listen(5000, () => {
